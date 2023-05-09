cec9e10bd324ad7767a98318c68fd2d66ec6e5d4
LukaLiegis <luka.liegis@gmail.com>
2023-05-09 16:12:15 +0100

    Commit

--- Arbitrage.py
+++ Arbitrage.py
@@ -2,7 +2,12 @@ import numpy as np
 import pandas as pd
 import ccxt
 
+# Getting Binance market data
 binance = ccxt.binance()
 market_binance = binance.load_markets()
-print(list(market_binance.items())[:3])
 
+# Getting OKX market data
+okx = ccxt.okx()
+okx_market = okx.load_markets()
+
+print(binance.fetch_ticker("DOGE/USDT"))
